6e368ac9c28384640252b45bdb5e0bd5dce2e868
alfly <alfly0023@gmail.com>
2023-01-12 16:44:12 +0800

    Add shell and zsh config

---
+++ shell/external.sh
@@ -0,0 +1,14 @@
+# # pip should only run if there is a virtualenv currently activated
+# export PIP_REQUIRE_VIRTUALENV=true
+
+# # Cache pip-installed packages to avoid re-downloading
+# export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+
+# # Python startup file
+# export PYTHONSTARTUP=$HOME/.pythonrc
+
+# # Vagrant
+# export VAGRANT_DISABLE_VBOXSYMLINKCREATE=1
+
+# # Docker
+# export DOCKER_SCAN_SUGGEST=false
