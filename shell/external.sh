6f5cfaca8d7707ce9a34948789874c179d43ba7f
alfly <alfly0023@gmail.com>
2023-10-14 16:32:11 +0800

    add condarc

--- shell/external.sh
+++ shell/external.sh
@@ -20,3 +20,25 @@ export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
 
 # gcc
 export CFLAGS="-Wall -O2 -g3 -pthread"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/lfe/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/lfe/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/lfe/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/lfe/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+
+# # Vagrant
+# export VAGRANT_DISABLE_VBOXSYMLINKCREATE=1
+
+# # Docker
+# export DOCKER_SCAN_SUGGEST=false
